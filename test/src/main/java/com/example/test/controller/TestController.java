4c8d0b52f8b4a4d91cc3dd2fa1ad8bb607015260
denggc3 <denggc3@chinaunicom.cn>
2019-12-02 10:45:57 +0800

    第一次提交

---
+++ test/src/main/java/com/example/test/controller/TestController.java
@@ -0,0 +1,35 @@
+package com.example.test.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+
+/**
+ * @Description: java类作用描述
+ * @Author: denggc3
+ * @CreateDate: 2019/12/2$ 10:31$
+ * @UpdateUser: 更新者
+ * @UpdateDate: 2019/12/2$ 10:31$
+ * @UpdateRemark: 更新内容
+ * @Version: 1.0
+ */
+
+
+@RestController
+@RequestMapping(value = "demo", method = RequestMethod.POST)
+public class TestController {
+
+    @RequestMapping(value = "test1", method = RequestMethod.POST)
+    public String test1(){
+        return "test1 success";
+    }
+
+
+    @RequestMapping(value = "test2", method = RequestMethod.POST)
+    public String test2(String name){
+        return name+"test2 success";
+    }
+}
+
